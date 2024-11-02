aadf493cb32d84a8cd94baff447460fc0f89a8ad
MithilSave <mithil.save@gmail.com>
2024-11-02 22:00:54 +0530

    Update room_page.dart

--- lib/Pages/Room Page/room_page.dart
+++ lib/Pages/Room Page/room_page.dart
@@ -8,211 +8,161 @@ class RoomPage extends StatelessWidget {
         centerTitle: true,
         title: Text('Rooms'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              margin: EdgeInsets.all(8.0),
-              padding: EdgeInsets.all(8.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                  borderRadius: BorderRadius.circular(8.0),
-                border: Border.all(color: Colors.blue.shade900),
-                shape: BoxShape.rectangle,
-                boxShadow: const[
-                    BoxShadow(
-                    color: Colors.grey,
-                    offset: Offset(0.0, 4.0), //(x,y)
-                    blurRadius: 8.0,
-                  ),
-                ],
-              ),
-              child: Row(
-                children: [
-                  Icon(
-                    Icons.location_on,
-                    color: Colors.blue.shade900,
-                  ),
-                  SizedBox(width: 8.0),
-                  Text(
-                      'Room 1',
-                  style:TextStyle(
-                    color: Colors.blue.shade900,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18
-                  )),
-                ],
+       body:  Center(
+        child: ListView(
+        children: [
+          Container(
+            margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+            padding: EdgeInsets.all(25),
+            width: 357.39,
+            height: 135.82,
+            decoration: ShapeDecoration(
+              color: Color(0xFF114C90),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.92),
               ),
             ),
-            Container(
-              margin: EdgeInsets.all(8.0),
-              padding: EdgeInsets.all(8.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(8.0),
-                border: Border.all(color: Colors.blue.shade900),
-                shape: BoxShape.rectangle,
-                boxShadow: const[
-                    BoxShadow(
-                    color: Colors.grey,
-                    offset: Offset(0.0, 4.0), //(x,y)
-                    blurRadius: 8.0,
-                  ),
-                ],
-              ),
-              child: Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.location_on,
-                    color: Colors.blue.shade900,
-                  ),
-                  SizedBox(width: 8.0),
-                  Text(
-                      'Room 2',
-                      style:TextStyle(
-                          color: Colors.blue.shade900,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18
-                      )),
-                ],
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    //name
+                    SizedBox(
+                      width: 70.02,
+                      height: 30.10,
+                      child: Text(
+                        'AIPPM',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.23,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w600,
+                          height: 0.09,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 6,
+                    ),
+                    SizedBox(
+                      width: 70.02,
+                      height: 30.10,
+                      child: Text(
+                        'CR 203',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.23,
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.w600,
+                          height: 0.09,
+                        ),
+                      ),
+                    ),
+
+                    SizedBox(
+                      width: 70.35,
+                      height: 17.80,
+                      child: Text(
+                        'Floor 2',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 19.65,
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.w600,
+                          height: 0.06,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Container(
+                  height: 70,
+                  width: 70,
+                  child: Image.asset('assets/icons/location.png'),
+                )
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+            padding: EdgeInsets.all(25),
+            width: 357.39,
+            height: 135.82,
+            decoration: ShapeDecoration(
+              color: Color(0xFF114C90),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.92),
               ),
             ),
-            Container(
-              margin: EdgeInsets.all(8.0),
-              padding: EdgeInsets.all(8.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(8.0),
-                border: Border.all(color: Colors.blue.shade900),
-                shape: BoxShape.rectangle,
-                boxShadow:const [
-                    BoxShadow(
-                    color: Colors.grey,
-                    offset: Offset(0.0, 4.0), //(x,y)
-                    blurRadius: 8.0,
-                  ),
-                ],
-              ),
-              child: Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.location_on,
-                    color: Colors.blue.shade900,
-                  ),
-                  SizedBox(width: 8.0),
-                  Text(
-                      'Room 3',
-                      style:TextStyle(
-                          color: Colors.blue.shade900,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18
-                      )),
-                ],
-              ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    //name
+                    SizedBox(
+                      width: 70.02,
+                      height: 30.10,
+                      child: Text(
+                        'AIPPM',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.23,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w600,
+                          height: 0.09,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 6,
+                    ),
+                    SizedBox(
+                      width: 70.02,
+                      height: 30.10,
+                      child: Text(
+                        'CR 203',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.23,
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.w600,
+                          height: 0.09,
+                        ),
+                      ),
+                    ),
+
+                    SizedBox(
+                      width: 70.35,
+                      height: 17.80,
+                      child: Text(
+                        'Floor 2',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 19.65,
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.w600,
+                          height: 0.06,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Container(
+                  height: 70,
+                  width: 70,
+                  child: Image.asset('assets/icons/location.png'),
+                )
+              ],
             ),
-            Container(
-              margin: EdgeInsets.all(8.0),
-              padding: EdgeInsets.all(8.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(8.0),
-                border: Border.all(color: Colors.blue.shade900),
-                shape: BoxShape.rectangle,
-                boxShadow: const[
-                    BoxShadow(
-                    color: Colors.grey,
-                    offset: Offset(0.0, 4.0), //(x,y)
-                    blurRadius: 8.0,
-                  ),
-                ],
-              ),
-              child: Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.location_on,
-                    color: Colors.blue.shade900,
-                  ),
-                  SizedBox(width: 8.0),
-                  Text(
-                      'Room 4',
-                      style:TextStyle(
-                          color: Colors.blue.shade900,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18
-                      )),
-                ],
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.all(8.0),
-              padding: EdgeInsets.all(8.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(8.0),
-                border: Border.all(color: Colors.blue.shade900),
-                shape: BoxShape.rectangle,
-                boxShadow: const [
-                     BoxShadow(
-                    color: Colors.grey,
-                    offset: Offset(0.0, 4.0), //(x,y)
-                    blurRadius: 8.0,
-                  ),
-                ],
-              ),
-              child: Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.location_on,
-                    color: Colors.blue.shade900,
-                  ),
-                  SizedBox(width: 8.0),
-                  Text(
-                      'Room 5',
-                      style:TextStyle(
-                          color: Colors.blue.shade900,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18
-                      )),
-                ],
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.all(8.0),
-              padding: EdgeInsets.all(8.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(8.0),
-                border: Border.all(color: Colors.blue.shade900),
-                shape: BoxShape.rectangle,
-                boxShadow: const [
-                    BoxShadow(
-                    color: Colors.grey,
-                    offset: Offset(0.0, 4.0), //(x,y)
-                    blurRadius: 8.0,
-                  ),
-                ],
-              ),
-              child: Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.location_on,
-                    color: Colors.blue.shade900,
-                  ),
-                  SizedBox(width: 8.0),
-                  Text(
-                      'Room 6',
-                      style:TextStyle(
-                          color: Colors.blue.shade900,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18
-                      )),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+          ),
+        ],
+    ),)
     );
   }
 }
