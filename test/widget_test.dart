6abf9d66e2a84e5d39b344e1e64340c116518471
MithilSave <mithil.save@gmail.com>
2024-11-05 22:13:32 +0530

    Launcher icon android

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:delego/Pages/main.dart';
+import 'package:delego/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
