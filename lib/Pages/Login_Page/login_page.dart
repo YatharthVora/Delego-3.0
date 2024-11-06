20647b260f81d60786affe2e9ee1539e5f9db32e
nnisarggada <gadannisarg@gmail.com>
2024-11-07 05:15:07 +0530

    FIX

--- lib/Pages/Login_Page/login_page.dart
+++ lib/Pages/Login_Page/login_page.dart
@@ -120,6 +120,16 @@ class _LoginPageState extends State<LoginPage> {
         );
 
         final data = json.decode(response.body);
+
+        if (data['detail'] == 'Please verify your email!') {
+          Navigator.of(context).pop();
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text(data['detail']),
+            backgroundColor: Colors.red,
+          ));
+          return;
+        }
+
         final id = data['id'] ?? '';
         final firstname = data['firstname'] ?? '';
         final lastname = data['lastname'] ?? '';
